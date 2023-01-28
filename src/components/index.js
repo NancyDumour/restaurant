23cc2b99761adf3e6ea96f598a2cb14d7b50aa6d
Nancy Dumour <nancydumour96@gmail.com>
2023-01-28 15:39:20 +0200

    Adding Footer Section

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 import SubHeading from "./SubHeading/SubHeading";
-import Newsletter from "./Footer/Newsletter";
+import Newsletter from "./Newsletter/Newsletter";
 import FooterOverlay from "./Footer/FooterOverlay";
 import MenuItem from "./Menuitem/MenuItem";
 import Navbar from "./Navbar/Navbar";
