f1900d8f09aa1e40fe230cf123d5c8289b008370
Nancy Dumour <nancydumour96@gmail.com>
2023-01-28 14:51:34 +0200

    Adding Awards Section

---
+++ src/components/AwardCard/AwardCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./AwardCard.css";
+
+const AwardCard = ({ award: { imgUrl, title, subtitle } }) => {
+  return (
+    <div className="app__laurels_awards-card">
+      <img src={imgUrl} alt="award" />
+      <div className="app__laurels-card_content">
+        <p className="p__cormorant" style={{ color: "#DCCA87" }}>
+          {title}
+        </p>
+        <p className="p__cormorant">{subtitle}</p>
+      </div>
+    </div>
+  );
+};
+
+export default AwardCard;
